3058e4b274a07d978879d0ab646ae6757004d35c
javier <javier.cozar@gmail.com>
2020-11-05 18:46:06 +0100

    implementing post to qr api

--- QRReader/Controllers/QRController.cs
+++ QRReader/Controllers/QRController.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -22,16 +19,22 @@ namespace QRReader.Controllers {
         [HttpGet("read-qr-code")]
         public async Task<string> ReadQrCode() {
             var apiUrl = configuration["QRReaderApiUrl"];
-            var content = GetContentFile();
+            using var content = GetContentFile();
             var response = await httpClient.PostAsync(apiUrl, content);
+            var text = await response.Content.ReadAsStringAsync();
 
-            return "";
+            return text;
         }
 
         private HttpContent GetContentFile() {
-            var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}/images/qrvode.jpeg";
-
-            throw new NotImplementedException();
+            var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}images/qrcode.jpeg";
+            var bytes = System.IO.File.ReadAllBytes(filePath);
+            var arrayContent = new ByteArrayContent(bytes);
+            var content = new MultipartFormDataContent() {
+                { arrayContent, "file", "qrcode.jpeg"}
+            };
+            
+            return content;
         }
     }
 }

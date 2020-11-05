a7ffc5aa55717347ff3fe4ba68f28d8cf501387b
javier <javier.cozar@gmail.com>
2020-11-05 19:24:11 +0100

    adding pathFile in method

--- QRReader/Controllers/QRController.cs
+++ QRReader/Controllers/QRController.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace QRReader.Controllers {
     [Route("api/[controller]")]
@@ -16,19 +17,24 @@ namespace QRReader.Controllers {
             this.configuration = configuration;
         }
 
-        [HttpGet("read-qr-code")]
-        public async Task<string> ReadQrCode() {
+        [HttpPost("read-qr-code")]
+        public async Task<string> ReadQrCode(string pathFile) {
             var apiUrl = configuration["QRReaderApiUrl"];
-            using var content = GetContentFile();
+            using var content = GetContentFile(pathFile);
             var response = await httpClient.PostAsync(apiUrl, content);
             var text = await response.Content.ReadAsStringAsync();
 
             return text;
         }
 
-        private HttpContent GetContentFile() {
-            var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}images/qrcode.jpeg";
-            var bytes = System.IO.File.ReadAllBytes(filePath);
+        private HttpContent GetContentFile(string pathFile) {
+            var fileInfo = new FileInfo(pathFile);
+
+            if (!fileInfo.Exists) {
+                throw new Exception("File doesn't exist");
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathFile);
             var arrayContent = new ByteArrayContent(bytes);
             var content = new MultipartFormDataContent() {
                 { arrayContent, "file", "qrcode.jpeg"}

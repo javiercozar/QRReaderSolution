bb0e9f43d7a76c33e6a3acc95f80acb5ed9b55a6
javier <javier.cozar@gmail.com>
2020-11-05 17:30:11 +0100

    .

--- QRReader/Controllers/QRController.cs
+++ QRReader/Controllers/QRController.cs
@@ -1,15 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
-namespace QRReader.Controllers
-{
+namespace QRReader.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class QRController : ControllerBase
-    {
+    public class QRController : ControllerBase {
+        readonly HttpClient httpClient;
+        private readonly IConfiguration configuration;
+         
+        public QRController(IConfiguration configuration) {
+            httpClient = new HttpClient();
+            this.configuration = configuration;
+        }
+
+        [HttpGet("read-qr-code")]
+        public async Task<string> ReadQrCode() {
+            var apiUrl = configuration["QRReaderApiUrl"];
+            var content = GetContentFile();
+            var response = await httpClient.PostAsync(apiUrl, null);
+
+            return "";
+        }
+
+        private object GetContentFile() {
+            throw new NotImplementedException();
+        }
     }
 }
